9746d7e49f07675e45627fd375c3f59442a3f5d9
remake <r_emak_e@naver.com>
2025-05-15 17:05:06 +0900

    mutiple

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -6,23 +6,28 @@ public class GradeCalculator {
         Student st = new Student();
         String name;
         int kor, eng, math;
+        int[] scores;
 
-        System.out.print("성적을 계산합니다.\n이름을 입력하세요 : ");
-        name = scanner.next();
+        for(int i = 0; i < 3; i++){
+            System.out.print("성적을 계산합니다.\n이름을 입력하세요 : ");
+            name = scanner.next();
 
-        System.out.print("국어 성적을 입력하세요 : ");
-        kor = scanner.nextInt();
+            System.out.print("국어 성적을 입력하세요 : ");
+            kor = scanner.nextInt();
 
-        System.out.print("영어 성적을 입력하세요 : ");
-        eng = scanner.nextInt();
+            System.out.print("영어 성적을 입력하세요 : ");
+            eng = scanner.nextInt();
 
-        System.out.print("수학 성적을 입력하세요 : ");
-        math = scanner.nextInt();
+            System.out.print("수학 성적을 입력하세요 : ");
+            math = scanner.nextInt();
 
-        int[] scores = new int[] { kor, eng, math };
+            scores = new int[] { kor, eng, math };
 
-        st.AddStudent(name, scores);
+            st.AddStudent(name, scores);
+        }
 
-        System.out.println("평균 : " + st.GetAverage() + ", 학점 : " + st.GetGrade());
+        for(Student sts : Student.List){
+            System.out.println("이름 : " + sts.GetName() + ", 평균 : " + sts.GetAverage() + ", 학점 : " + sts.GetGrade());
+        }
     }
 }
